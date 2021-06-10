4adbc9b7973ecde766d252007c68b37a5b3bcf5f
Daniel Ribeiro Santiago <daniel.santiago@zup.com.br>
2021-06-10 14:23:47 -0300

    Adiciona as configurações necessárias para o funcionamento correto do keycloak com a nossa aplicação

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,10 @@ FROM maven:3.6.3-jdk-11 AS builder
 WORKDIR /usr/src/app
 COPY pom.xml .
 RUN mvn dependency:resolve
+RUN mvn dependency:resolve-plugins
 COPY src ./src
 RUN mvn package -DskipTests
 
 FROM adoptopenjdk/openjdk11:alpine
-RUN addgroup -S spring && adduser -S spring
-USER spring:spring
 COPY --from=builder /usr/src/app/target/proposta-microservice.jar /usr/app/app.jar
 ENTRYPOINT ["java","-jar","/usr/app/app.jar"]
--- src/main/java/br/com/zup/zupacademy/daniel/proposta/common/configuration/security/SecurityConfigurations.java
+++ src/main/java/br/com/zup/zupacademy/daniel/proposta/common/configuration/security/SecurityConfigurations.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @EnableWebSecurity
 @Configuration
@@ -13,14 +14,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
     // Configurações de Autorização (diz quem pode acessar cada url, perfis de aceso etc)
     @Override
         protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-
-        .antMatchers("/**")
-        .permitAll()
-
-        .anyRequest()
-        .authenticated()
-
-        .and().csrf().disable() ;
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                        .antMatchers(HttpMethod.GET,"/proposta/**").hasAuthority("SCOPE_proposta:read")
+                        .antMatchers(HttpMethod.POST,"/proposta/**").hasAuthority("SCOPE_proposta:write")
+                        .antMatchers(HttpMethod.POST,"/biometria/**").hasAuthority("SCOPE_biometria:write")
+                        .anyRequest().permitAll()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
